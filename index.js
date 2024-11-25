1d88dac9159957682ce131cfa16f1a79a87059db
Anthony Lorenzo <anth.coding@gmail.com>
2024-11-25 16:31:11 -0500

    Timer APP

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-let x = { value: 10 };
+// function Circle(radius) {
+//   this.radius = radius;
 
-// Since object are Refence Types
-// What I'm doing here is copying the reference address not the actual value of the object
-let y = x;
+//   let defaultLocation = { x: 0, y: 0 };
 
-x.value = 20;
+//   this.draw = function () {
+//     console.log('Draw with radius: ', this.radius);
+//   };
 
-let obj = { value: 1 };
+//   Object.defineProperty(this, 'dafaultLocaiton', {
+//     get: function () {
+//       return defaultLocation;
+//     },
+//     set: function (value) {
+//       return defaultLocation = value;
+//     },
+//   });
+// }
 
-function increase(obj) {
-  obj.value++;
+// const circle = new Circle(10);
+// console.log(circle.defaultLocation);
+
+function StopWatch() {
+  let duration = 0;
+  let startTime = 0;
+  let endTime = 0;
+  let running = false;
+
+  this.start = function () {
+    if (running) {
+      throw new Error('StopWach is running');
+    }
+    startTime = new Date();
+    running = true;
+  };
+
+  this.stop = function () {
+    if (!running) {
+      throw new Error('StopWach is not running');
+    }
+    endTime = new Date();
+    seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+    running = false;
+  };
+
+  this.reset = function () {
+    duration = 0;
+  };
+
+  Object.defineProperty(this, 'duration', {
+    get: function () {
+      return duration;
+    },
+  });
 }
-increase(obj);
-console.log(obj);
+
+const stopWatch = new StopWatch();
